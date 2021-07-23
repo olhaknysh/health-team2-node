5dc5e29db44ae14e39dd3b0b7c5bb1ff59baaade
olhaknysh <olhaknysh1994@gmail.com>
2021-07-23 16:52:12 +0300

    add base files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    env: {
-        commonjs: true,
-        es2021: true,
-        node: true
-    },
-    extends: ['standard', 'prettier'],
-    parserOptions: {
-        ecmaVersion: 12
-    },
-    rules: {
-        'comma-dangle': 'error',
-        'space-before-function-paren': 'off'
-    }
-}
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'plugin:json/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
+    'no-throw-literal': 'off',
+  },
+};
