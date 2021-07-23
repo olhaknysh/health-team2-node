4b1063375f765d47a8c0eddffcb348cd49e013d5
yurii-shendryk <yuriishendryk93@gmail.com>
2021-07-24 01:31:45 +0300

    add auth controllers

---
+++ controllers/usersController.js
@@ -0,0 +1,41 @@
+const { statusCode } = require('../helpers/constants');
+
+const { signup, loginUser } = require('../services/authService');
+
+const { updateToken } = require('../services/usersService');
+
+const signupUserController = async (req, res) => {
+  const { name, login, password } = req.body;
+  const createdUser = await signup(name, login, password);
+  res.status(statusCode.CREATED).json({
+    user: {
+      name: createdUser.name,
+      login: createdUser.login,
+    },
+  });
+};
+
+const loginUserController = async (req, res) => {
+  const { login, password } = req.body;
+  const loggedInUser = await loginUser(login, password);
+  res.status(statusCode.OK).json({
+    token: loggedInUser.token,
+    user: {
+      name: loggedInUser.name,
+      login: loggedInUser.login,
+    },
+  });
+};
+
+const logoutUserController = async (req, res) => {
+  const id = req.user._id;
+  const { token } = req.user;
+  await updateToken(id, token);
+  res.status(statusCode.NO_CONTENT).json({});
+};
+
+module.exports = {
+  signupUserController,
+  loginUserController,
+  logoutUserController,
+};
