fb1f968a2a57bf71c8588917a51a8d5b29961c06
yurii-shendryk <yuriishendryk93@gmail.com>
2021-07-28 01:38:46 +0300

    add removeUserProductById function

--- services/userProductsService.js
+++ services/userProductsService.js
@@ -1,5 +1,7 @@
 const { Product } = require('../model/productModel');
 const { UserProduct } = require('../model/userProductsModel');
+const { statusCode } = require('../helpers/constants');
+const { CustomError } = require('../helpers/errors');
 
 let totalCalories = 0;
 
@@ -16,9 +18,38 @@ const addUserProduct = async (userId, body) => {
     },
     { new: true, upsert: true },
   );
+  if (!result) {
+    throw new CustomError(statusCode.NOT_FOUND, 'Not found');
+  }
+  return result;
+};
+
+const removeUserProductById = async (userId, productId, title) => {
+  const ProductsWithSameTitle = await Product.findOne({ 'title.ru': title });
+  let { totalCalories } = await UserProduct.findOne(
+    { products: { $elemMatch: { title } } },
+    { totalCalories: 1, _id: 0 },
+  );
+
+  totalCalories -= ProductsWithSameTitle.calories;
+  const result = await UserProduct.findOneAndUpdate(
+    {
+      products: { $elemMatch: { _id: productId } },
+      userId,
+    },
+    {
+      totalCalories,
+      $pull: { products: { _id: productId } },
+    },
+    { new: true },
+  );
+  if (!result) {
+    throw new CustomError(statusCode.NOT_FOUND, 'Not found');
+  }
   return result;
 };
 
 module.exports = {
   addUserProduct,
+  removeUserProductById,
 };
