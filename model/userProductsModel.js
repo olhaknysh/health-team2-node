25be80a9155d46ecad67ea2fd7d56ccc9211f484
yurii-shendryk <yuriishendryk93@gmail.com>
2021-07-27 12:21:24 +0300

    add Schema

---
+++ model/userProductsModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userProductsSchema = new Schema(
+  {
+    totalCalories: {
+      type: Number,
+    },
+    date: {
+      type: Date,
+    },
+    products: {
+      type: [Object],
+    },
+    userID: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+  },
+  { versionKey: false },
+);
+
+const UserProduct = mongoose.model('userProduct', userProductsSchema);
+
+module.exports = { UserProduct };
