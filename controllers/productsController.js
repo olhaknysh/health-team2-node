7acc4fe478c91ad75b15bbe096a9dfa1bb1439f4
yurii-shendryk <yuriishendryk93@gmail.com>
2021-07-28 15:21:07 +0300

    update res objects in controllers

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -10,27 +10,23 @@ const getProductsByQueryContorller = async (req, res) => {
   const { query } = req;
   const products = await getProductsByQuery(query);
 
-  res.status(statusCode.OK).json({ ...products });
+  res.status(statusCode.OK).json(products);
 };
 
 const addUserProductController = async (req, res) => {
   const userId = req.user._id;
   const { body } = req;
   const result = await addUserProduct(userId, body);
-  res.status(statusCode.CREATED).json({
-    result,
-  });
+  res.status(statusCode.CREATED).json(result);
 };
 
 const removeUserProductController = async (req, res) => {
   const userId = req.user._id;
   const { productId } = req.params;
-  const { title } = req.body;
-  const result = await removeUserProductById(userId, productId, title);
-  res.status(statusCode.OK).json({
-    message: 'product deleted',
-    totalCalories: result.totalCalories,
-  });
+  const result = await removeUserProductById(userId, productId);
+  res
+    .status(statusCode.OK)
+    .json({ message: 'product deleted', totalCalories: result.totalCalories });
 };
 
 module.exports = {
